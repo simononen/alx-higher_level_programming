85f9a63e85a5c8df7fe2d9836fbc37af1af17ce2
Simon Onen <sonen8524@gmail.com>
2022-06-21 13:11:39 -0700

    Add 3-square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module for working with squares
+"""
+
+
+class Square:
+    """Represent a 2D Polygon with 4 equal and perpendicular sides
+    """
+    def __init__(self, size=0):
+        """Initialize square with a given size
+        Args:
+            size (int): Size of the square
+        """
+        super().__init__()
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        else:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+
+    def area(self):
+        """Compute Area of this square
+        Returns:
+            int: Area of the square
+        """
+        return self.__size ** 2
