851bd59a9ed49877cf4ab2fbda52be4c2fdb5188
Simon Onen <sonen8524@gmail.com>
2022-06-30 13:20:01 -0700

    Add 6-max_integer_test.py

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+unittest module for the max_integer function
+'''
+import unittest
+from importlib import import_module
+
+
+class TestMaxInteger(unittest.TestCase):
+    '''
+    Test the max_integer function
+    '''
+    def test_area(self):
+        '''
+        The equality tests for the max_integer function
+        '''
+        max_integer = import_module('6-max_integer', '..').max_integer
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([7, 2, -3, 45, 6]), 45)
+        self.assertEqual(max_integer([7, 2, 45, 6]), 45)
+        self.assertEqual(max_integer([-7, -2, -45, -6]), -2)
+        self.assertEqual(max_integer([-7, 1, -18, -6]), 1)
