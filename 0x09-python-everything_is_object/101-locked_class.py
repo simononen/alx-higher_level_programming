6deb6085409733d3267787ff006b4c5290b92c5c
Simon Onen <sonen8524@gmail.com>
2022-07-04 12:56:37 -0700

    Add 103-line2.txt

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,6 +6,6 @@ Module contains a class with restrictions
 
 class LockedClass:
     '''
-	Represent a class with restricted attribute modification.
+	Represent class with restricted attribute modification
     '''
     __slots__ = ['first_name']
